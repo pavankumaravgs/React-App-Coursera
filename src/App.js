8d6216cfb6931588054ab97f89ff27ca0d60fac0
pavankumaravgs <pavankumaravgs@github.com>
2020-06-18 14:41:43 +0530

    menu component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Menu from './components/MenuComponent'
 import {Navbar , NavbarBrand} from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color='secondary'>
         <div className='container'>
           <NavbarBrand href='/'>
@@ -13,6 +14,7 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
